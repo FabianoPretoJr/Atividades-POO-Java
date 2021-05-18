31cc647b8e61d2acd74b9bce76b7bcd2ddcc5d70
FabianoPretoJr <61958796+FabianoPretoJr@users.noreply.github.com>
2021-05-17 22:12:12 -0300

    Subindo atividade 21

---
+++ Atividade 21/src/FormaGeometrica.java
@@ -0,0 +1,16 @@
+public abstract class FormaGeometrica {
+    protected int lados;
+    public void setLados(int lados)
+    {
+        if(lados < 0) this.lados = 0;
+        else this.lados = lados;
+    }
+
+    public abstract double area();
+
+    public void imprime()
+    {
+        System.out.println("Número de lados: " + this.lados);
+        System.out.printf("Área: %.2f\n", area());
+    }
+}
